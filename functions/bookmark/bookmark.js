395708e17b5c821653ed33f6579c8faa604b4cf3
laherasif <laherasif@gmail.com>
2020-11-30 02:33:09 -0800

    new chnage

--- functions/bookmark/bookmark.js
+++ functions/bookmark/bookmark.js
@@ -1,7 +1,5 @@
 const { ApolloServer, gql } = require('apollo-server-lambda')
-require("dotenv").config({
-  path: `.env`,
-})
+
 
 var faunadb = require('faunadb'),
   q = faunadb.query;
@@ -26,7 +24,7 @@ const typeDefs = gql`
   }
   `
 // updateTodo(task : String! , status : Boolean!):Todo
-var adminClient = new faunadb.Client({ secret: process.env.FAUNA_SECRET_KY });
+var adminClient = new faunadb.Client({ secret: 'fnAD6OD-QyACBcMFsavYmk2L8OkTxK5zWMj2r_Y9' });
 
 const resolvers = {
   Query: {
